60945a5e048fd887e8596a4255072aba2a480a70
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-03 18:46:45 +0200

    tailwind, vite, pinia and some basic files were updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    environment: "jsdom",
+  },
+});
