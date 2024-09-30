2d3eff216f3a6f7e9df077bc05f676a8bd6a10e8
aitorgarciafernandez <aitorgarciafernandezcv@gmail.com>
2024-09-30 19:26:24 +0200

    router fixed

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
+<template>
+  <div id="app">
+    <header>
+      <NavBar />
+    </header>
+    <router-view />
+  </div>
+</template>
+
 <script setup>
 import NavBar from "./components/Global/Navbar/NavBar.vue";
-import Carousel from "./components/Home/Carousel.vue";
-import StoreFeatures from "./components/Home/StoreFeatures.vue";
-import NewsLetter from "./components/Global/NewsLetter.vue";
-import Categories from "./components/Home/Categories.vue";
-import Recommended from "./components/Home/Recommended.vue";
-import ProductsCarousel from "./components/Home/ProductsCarousel.vue";
-import FooterDiv from "./components/Global/FooterDiv.vue";
 </script>
-
-<template>
-  <header>
-    <NavBar />
-    <Carousel />
-  </header>
-
-  <main class="bg-white">
-    <NewsLetter />
-
-    <StoreFeatures />
-
-    <ProductsCarousel />
-    <FooterDiv />
-  </main>
-</template>
 
 <style scoped>
 body {
   background-color: black;
+}
+
+header {
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import Carousel from "../components/Home/Carousel.vue";
+import StoreFeatures from "../components/Home/StoreFeatures.vue";
+import NewsLetter from "../components/Global/NewsLetter.vue";
+import ProductsCarousel from "../components/Home/ProductsCarousel.vue";
+import FooterDiv from "../components/Global/FooterDiv.vue";
+</script>
 
 <template>
-  <header></header>
+  <Carousel />
+
+  <NewsLetter />
+
+  <StoreFeatures />
+
+  <ProductsCarousel />
+  <FooterDiv />
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  background-color: black;
+}
+</style>
